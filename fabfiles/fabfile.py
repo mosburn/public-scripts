d96032bd8d2c27af01f7031ce0376eda0e40bad2
Michael Osburn <michael@mosburn.com>
2013-11-19 10:40:06 -0700

    adding a section for prepping an osx system

--- fabfiles/fabfile.py
+++ fabfiles/fabfile.py
@@ -37,3 +37,52 @@ def post_install_linux():
         run('git clone git@github.com:mosburn/dotfiles.git')
     with cd('/home/%s/Projects/dotfiles' % env.user):
         run('sh install_dotfiles')
+
+
+def install_osx():
+    """
+        Installs base OSX settings to speed up development.
+        This logic of this is to install python related components,
+        sets up git, and installs some base configurations
+
+        :requires xcode: Requires XCode to compile the python modules
+
+    """
+
+    """Python bashrc configuration"""
+    if not files.contains('~/.bashrc',
+                          '#pip should only run if there is a virtualenv \
+                            currently activated'):
+        files.append('~/.bashrc',
+                     'export PIP_REQUIRE_VIRTUALENV=true')
+    if not files.contains('~/.bashrc',
+                          'export PIP_REQUIRE_VIRTUALENV=true'):
+        files.append('~/.bashrc',
+                     '#pip should only run if there is a virtualenv currently \
+                     activated')
+    if not files.contains('~/.bashrc',
+                          '# cache pip-installed packages to avoid \
+                          re-downloading'):
+        files.append('~/.bashrc',
+                     '# cache pip-installed packages to avoid re-downloading')
+    if not files.contains('~/.bashrc',
+                          'export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache'):
+        files.append('~/.bashrc',
+                     'export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache')
+    if not files.contains('~/.bashrc',
+                          'export WORKON_HOME=$HOME/.virtualenvs'):
+        files.append('~/.bashrc',
+                     'export WORKON_HOME=$HOME/.virtualenvs')
+    if not files.contains('~/.bashrc',
+                          'export PROJECT_HOME=$HOME/Projects'):
+        files.append('~/.bashrc',
+                     'export PROJECT_HOME=$HOME/Projects')
+    if not files.contains('~/.bashrc',
+                          'source /usr/local/bin/virtualenvwrapper.sh'):
+        files.append('~/.bashrc',
+                     'source /usr/local/bin/virtualenvwrapper.sh')
+
+    """Check for existance of a ssh key, if not it creates it"""
+    if not files.exists("~/.ssh/id_rsa.pub"):
+        """Generate the new key"""
+        run('ssh-keygen -t rsa -b 4096 ')
